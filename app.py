f8b7ad51349364bbdb15a261e303b4eef9bbcd05
alphagithuku5-cmyk <alphagithuku5@gmail.com>
2026-02-07 17:30:17 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,1 @@
+Google health tells what a diseases is AfyaRafiki tells you where to go and how to get there.desinged for the kenyan context,this AI partner understands sympotmos in sheng or swahili instantley maps the nearest open hospital with avalilable specialist
